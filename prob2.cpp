d15c98a7619f7030f9f3a226e9cae98ced0a0b69
User <puppy955101@gmail.com>
2020-05-21 10:26:20 +0800

    prob2

---
+++ prob2.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define LL long long
+#define pb push_back
+int main(){
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);
+	LL input;
+	cin >> input;
+	cout << input-input/3-input/5+input/15*2 << '\n';
+}
+
